a42311f6f989a1e6afbf50e453236fade43510c7
Jhon Oliver <johnoliver021@gmail.com>
2020-09-10 21:06:37 -0300

    first commit

---
+++ src/store/index.ts
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import cart from './modules/cart/reducer';
+import rootReducer from './modules/rootReducer';
+const store = createStore(rootReducer);
+
+export default store;
