b415aa27138d8d84aa075b96217b2ef24f8493f7
Jhon Oliver <johnoliver021@gmail.com>
2020-09-15 21:11:13 -0300

    incrementando quantidade do carrinho

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 import { IProduct } from '../store/modules/cart/types';
 import api from '../services/api';
-import { addProductToCart } from '../store/modules/cart/actions';
-
-// import { Container } from './styles';
+import CatalogItem from './CatalogItem';
 
 const Catalog: React.FC = () => {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -15,24 +11,11 @@ const Catalog: React.FC = () => {
       setCatalog(response.data);
     });
   }, []);
-  const handleAddProductToCart = useCallback(
-    (product: IProduct) => {
-      dispatch(addProductToCart(product));
-    },
-    [dispatch],
-  );
   return (
     <main>
       <h1>Catalog</h1>
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong>
-          {' - '}
-          <span>{product.price}</span>{' '}
-          <button type="button" onClick={() => handleAddProductToCart(product)}>
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   );
