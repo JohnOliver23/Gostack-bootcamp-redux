e5373649391094ee3ba1fb50d7240be4f52a0b3b
Jhon Oliver <johnoliver021@gmail.com>
2020-09-16 20:46:49 -0300

    criando redux-saga para checar a quantidade de stock do item

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { IProduct } from '../store/modules/cart/types';
-import { addProductToCart } from '../store/modules/cart/actions';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { useDispatch } from 'react-redux';
 
 interface CatalogItemProps {
@@ -10,7 +10,7 @@ interface CatalogItemProps {
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProductToCart(product));
+    dispatch(addProductToCartRequest(product));
   }, [dispatch, product]);
   return (
     <article>
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,6 +1,7 @@
 import { Reducer } from 'redux';
 import produce from 'immer';
 import { ICartState } from './types';
+import { Console } from 'console';
 
 const INITIAL_STATE: ICartState = {
   items: [],
@@ -8,7 +9,7 @@ const INITIAL_STATE: ICartState = {
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   return produce(state, draft => {
     switch (action.type) {
-      case 'ADD_PRODUCT_TO_CART': {
+      case 'ADD_PRODUCT_TO_CART_SUCCESS': {
         const { product } = action.payload;
 
         const productInCartIndex = draft.items.findIndex(
@@ -24,6 +25,9 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
           });
         }
         break;
+      }
+      case 'ADD_PRODUCT_TO_CART_FAILURE': {
+        console.log('failure', action.payload);
       }
       default: {
         return draft;
